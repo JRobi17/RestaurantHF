f1d3ad1ae0a1c277352a1401c68ca9b7f6b8be71
JRobi17 <Janrobi2000@gmail.com>
2021-11-28 11:23:37 +0100

    Basic order implementation

---
+++ frontend/src/app/inplace/inplace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inplace',
+  templateUrl: './inplace.component.html',
+  styleUrls: ['./inplace.component.css']
+})
+export class InplaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
